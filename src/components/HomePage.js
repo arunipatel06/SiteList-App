d02dff468fbb2d9447707909094957d8fe0b22ef
arunipatel06 <arunipatel06@gmail.com>
2021-02-04 20:45:08 -0500

    file restructure

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+//components
+import SiteList from "./SiteList";
+
+const HomePage = () => {
+  return (
+    <div>
+      <SiteList />
+    </div>
+  );
+};
+
+export default HomePage;
