d69efe11581fd34032411b844d67e9ea14a4acb8
arunipatel06 <arunipatel06@gmail.com>
2021-02-07 23:21:12 -0500

    create homepage route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+/* eslint-disable no-useless-constructor */
+import { Component } from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import SitePage from "./components/SitePage";
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div style={{ width: "100%", overflowX: "hidden" }}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/site/:id" component={SitePage} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
